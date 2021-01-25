c3be298265916676d5041b8c8525c56a243c7c59
dmintz7 <dmintz37@gmail.com>
2021-01-25 11:05:57 -0500

    Update run.py

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from logging.handlers import RotatingFileHandler
 from decimal import Decimal, ROUND_HALF_UP, ROUND_HALF_DOWN
 from plexapi.server import PlexServer
 
-folder = os.path.dirname(os.path.abspath(__file__)).split("/")[-1] if if folder !- "" else "Unknown"
+folder = os.path.dirname(os.path.abspath(__file__)).split("/")[-1] if folder !- "" else "Unknown"
 extra = {'folder_name': os.path.dirname(os.path.abspath(__file__)).split("/")[-1]}
 formatter = logging.Formatter('%(asctime)s - %(levelname)10s - %(folder_name)15s:%(module)15s:%(funcName)30s:%(lineno)5s - %(message)s')
 logger = logging.getLogger()
