3e9f67f7270476ac555ee63247557f7ecb173272
dmintz7 <dmintz37@gmail.com>
2021-01-24 23:34:53 -0500

    Update Log Format

--- run.py
+++ run.py
@@ -3,8 +3,8 @@ from logging.handlers import RotatingFileHandler
 from decimal import Decimal, ROUND_HALF_UP, ROUND_HALF_DOWN
 from plexapi.server import PlexServer
 
-filename, file_extension = os.path.splitext(os.path.basename(__file__))
-formatter = logging.Formatter('%(asctime)s - %(levelname)10s - %(module)15s:%(funcName)30s:%(lineno)5s - %(message)s')
+extra = {'folder_name': os.path.dirname(os.path.abspath(__file__)).split("/")[-1]}
+formatter = logging.Formatter('%(asctime)s - %(levelname)10s - %(folder_name)15s:%(module)15s:%(funcName)30s:%(lineno)5s - %(message)s')
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 consoleHandler = logging.StreamHandler(sys.stdout)
@@ -15,6 +15,9 @@ logger.setLevel(config.LOG_LEVEL)
 fileHandler = RotatingFileHandler(config.LOG_FOLDER + '/Omni.log', maxBytes=1024 * 1024 * 1, backupCount=1)
 fileHandler.setFormatter(formatter)
 logger.addHandler(fileHandler)
+logger = logging.LoggerAdapter(logger, extra)
+
+
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 sdr = sodarr.API(config.sonarr_host + '/api', config.sonarr_api)
